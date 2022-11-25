0d4da37f432da48019451245eb589da5e444ca2f
imbard <nicota1234@gmail.com>
2022-11-25 00:33:52 -0300

    :construction: Desafio 5

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask
 from routes import main
-# from db import db
+from db import db
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:@localhost/ds4"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:Atlatica&0805@localhost/ds4"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
-# db.init_app(app)
+db.init_app(app)
 app.register_blueprint(main)
 
 if __name__ == "__main__": 
-  app.run(debug=True)
+  app.run(debug=True) 
--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
 from models.contatos import Contatos
-# from db import db
+from db import db
 
 main = Blueprint('routes', __name__)
 
@@ -8,29 +8,30 @@ main = Blueprint('routes', __name__)
 def home():
     return render_template("index.html")
 
-@main.route("/contato")
-def cont():
-    return render_template("contato.html")
-
-@main.route("/who", methods=["GET", "POST"])
+@main.route("/who")
 def who():
-    # if request.method == "POST":
-    #     email = request.form['email']
-    #     assunto = request.form['assunto']
-    #     descricao = request.form['comment']
-
-    #     insert = Contatos(
-    #         email = email,
-    #         assunto = assunto,
-    #         descricao = descricao
-    #     )
-
-    #     db.session.add(insert)
-    #     db.session.commit()
-
     return render_template("who.html")
 
+@main.route("/contato", methods=["GET", "POST"])
+def cont():
+    if request.method == "POST":
+        email = request.form['email']
+        assunto = request.form['assunto']
+        descricao = request.form['comment']
+
+        insert = Contatos(
+            email = email,
+            assunto = assunto,
+            descricao = descricao
+        )
+
+        db.session.add(insert)
+        db.session.commit()
+        return redirect(url_for('routes.list'))
+    return render_template("contato.html")
+
+
 @main.route('/list')
-def list():
+def list(): 
     data = Contatos.query.all()
     return render_template('list.html', data=data)
